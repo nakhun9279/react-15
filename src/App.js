b765b7951af9ffebe2e876554c93192f626bf5f7
nakhun9279 <fjfj9279@naver.com>
2020-08-31 13:54:09 +0900

    15.3.3 색상 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
 import { ColorProvider } from './contexts/color';
+import SelectColors from './components/SelectColors';
+ 
 const App = () => {
   return (
-    <ColorContext.Provider value={{ color: 'red' }}>
+    <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 };
-
-
+ 
 export default App;
