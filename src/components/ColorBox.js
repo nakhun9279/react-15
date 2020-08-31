407c04b431b234d20e7708ef9f84be93512ea484
nakhun9279 <fjfj9279@naver.com>
2020-08-31 14:00:51 +0900

    15.4.2 static contexttype사용하기

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,26 +1,36 @@
-import React, { useContext } from 'react';
+import React, { Component } from 'react';
 import ColorContext from '../contexts/color';
- 
-const ColorBox = () => {
-  const { state } = useContext(ColorContext);
-  return (
-    <>
-      <div
-        style={{
-          width: '64px',
-          height: '64px',
-          background: state.color
-        }}
-      />
-      <div
-        style={{
-          width: '32px',
-          height: '32px',
-          background: state.subcolor
-        }}
-      />
-    </>
-  );
-};
- 
-export default ColorBox;
+
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+
+
+class SelectColors extends Component {
+  static contextType = ColorContext;
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+        <div style={{ display: 'flex' }}>
+          {colors.map(color => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer'
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+
+
+export default SelectColors;
